f1d734799e4a4581f7395eeb0cae08cf62463ce8
Piotr Kulesza <piotrkulesza16@onet.pl>
2018-01-23 14:47:44 +0100

    Modyfikacja

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //#Piotr
 int Dodawanie();int mnozenie();int odejmowani();
 
-int main{
+int main(){
 int wynik,liczba;
 cout<<"1<==Dodawanie"<<endl;
 cout<<"2<==Odejmowanie"<<endl;
@@ -15,14 +15,14 @@ cout<<"3<==Mnożenie"<<endl;
 cout<<"4<==Wyjście"<<endl;
 
 do{
-switch(wybor){
+switch(liczba){
 case 1: wynik=Dodawanie();cout<<wynik<<endl;system("pause");system("cls");break;
 case 2: wynik=odejmowani();cout<<wynik<<endl;system("pause");system("cls");break;
 case 3: wynik=mnozenie();cout<<wynik<<endl;system("pause");system("cls");break;
 default:system("cls");break;
 }
 
-}while(wybor!=4);
+}while(liczba!=4);
 
 
 
