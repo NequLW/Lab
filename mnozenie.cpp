15cb28990903e142e2d98e53fece17ccba9d5f3e
Marcin Knap <markna000utp.edu.pl>
2018-01-23 13:33:17 +0100

    marcin

--- mnozenie.cpp
+++ mnozenie.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int mnozenie()
 {
+setlocale(LC_ALL,"";
 int a,b;
-cout<<"Podaj liczby do mnozenia"<<endl;
+cout<<"Podaj liczby do mnożenia"<<endl;
 cin>>a:
 cin>>b:
 cout<<"Wynik mnożenia"<<endl;
