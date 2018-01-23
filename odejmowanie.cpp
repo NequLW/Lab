aee636ab8debfbd9dbf6703784a431e5bdd12142
Kacper <kacdur003@utp.edu.pl>
2018-01-23 13:36:38 +0100

    durol

--- odejmowanie.cpp
+++ odejmowanie.cpp
@@ -1,15 +1,17 @@
 #include<ostream>
 using namespace std;
 
-int main{
+int odejmowani()
 
+  setlocale(LC_ALL, "");
+  
 int b;
 int a;
 cout<<"Podaj a: "
 cin>> a;
 cout<<"Podaj b: "
 cin>> b;
-cout<<"Wynik odejmowania to "<<a-b;
+cout<<"Wynik odejmowania to "<< a-b;
 
 
 return 0;
