2b1f4900ded2441ce112be2fa87cc5075d5c30e2
NequLW <mackor002@utp.edu.pl>
2018-01-23 13:18:32 +0100

    czwarty commit

--- Dodawanie.cpp
+++ Dodawanie.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+using namespace std;
 int Dodawanie()
 {
 int a,b;
-cout<<"Podaj 2 liczby:"
+cout<<"Podaj 2 liczby:";
 cin>>a;
 cout<<endl;
 cin>>b;
-    cout<<"Wynik dodawania to: "<<a+b;
+    cout<<"Wynik dodawania to: "<<endl;
+	return a+b;
 
 }
